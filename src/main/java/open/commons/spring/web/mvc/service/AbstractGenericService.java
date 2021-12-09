9bb46ee9155c4d9ff109673b22717fd6f476b226
Park, Jun-Hong <parkjunhong77@gmail.com>
2021-12-09 16:49:42 +0900

    데이터 변환 함수 추가.

--- src/main/java/open/commons/spring/web/mvc/service/AbstractGenericService.java
+++ src/main/java/open/commons/spring/web/mvc/service/AbstractGenericService.java
@@ -362,13 +362,59 @@ public abstract class AbstractGenericService extends AbstractComponent implement
      *            데이터를 읽기 위한 시작 위치 (0부터 시작)
      * @param limit
      *            읽을 데이터 개수
+     * @param orderByArgs
+     *            정렬 기준.<br>
+     *            <b>데이터 정의</b><br>
+     *            <li>포맷: {column} {direction}<br>
+     *            <li>예: name asc
+     * @param dtoType
+     *            DTO class
+     * @param converter
+     *            Entity -> DTO 변환 함수
      * @return
      *
      * @since 2021. 12. 6.
-     * @author Park_Jun_Hong_(fafanmama_at_naver_com) 정렬 기준.<br>
-     *         <b>데이터 정의</b><br>
-     *         <li>포맷: {column} {direction}<br>
-     *         <li>예: name asc
+     * @version 0.4.0
+     * @author Park_Jun_Hong_(fafanmama_at_naver_com)
+     */
+    protected <E, D> Result<List<D>> selectMulti(SearchResultType type //
+            , Function<String[], Result<List<E>>> funcAll //
+            , TripleFunction<Integer, Integer, String[], Result<List<E>>> funcPagination, int offset, int limit //
+            , String[] orderByArgs //
+            , Class<D> dtoType, Function<E, D> converter) {
+        return convertMultiResult(selectMulti(type, funcAll, funcPagination, offset, limit, orderByArgs), dtoType, converter);
+    }
+
+    /**
+     * 
+     * <br>
+     * 
+     * <pre>
+     * [개정이력]
+     *      날짜      | 작성자   |   내용
+     * ------------------------------------------
+     * 2021. 12. 6.     박준홍         최초 작성
+     * </pre>
+     *
+     * @param <E>
+     *            Table Entity 타입
+     * @param <D>
+     *            DTO 타입
+     * @param type
+     *            검색 유형.
+     * @param funcAll
+     *            전체 검색 함수
+     * @param funcPagination
+     *            Pagination 검색 함수
+     * @param offset
+     *            데이터를 읽기 위한 시작 위치 (0부터 시작)
+     * @param limit
+     *            읽을 데이터 개수
+     * @return
+     *
+     * @since 2021. 12. 6.
+     * @version 0.4.0
+     * @author Park_Jun_Hong_(fafanmama_at_naver_com)
      */
     protected <E> Result<List<E>> selectMulti(SearchResultType type //
             , Supplier<Result<List<E>>> funcAll //
@@ -416,6 +462,7 @@ public abstract class AbstractGenericService extends AbstractComponent implement
      * @return
      *
      * @since 2021. 12. 6.
+     * @version 0.4.0
      * @author Park_Jun_Hong_(fafanmama_at_naver_com)
      */
     protected <E, D> Result<List<D>> selectMulti(SearchResultType type //
